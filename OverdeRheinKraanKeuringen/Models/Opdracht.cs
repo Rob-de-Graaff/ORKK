7585c373c2e038fc3d5d0323c8bfeea6d63b2e8e
EyesMice <42554848+EyesMice@users.noreply.github.com>
2020-03-24 11:48:22 +0100

    Opdracht class aangemaakt

---
+++ OverdeRheinKraanKeuringen/Models/Opdracht.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OverdeRheinKraanKeuringen.Models
+{
+    public class Opdracht
+    {
+        public int opdrachtNummer { get; set; }
+        public string werkIntrustie { get; set; }
+        public DateTime DatumUitvoering { get; set; }
+        public string kabelLeverancier { get; set; }
+        public string Waarnemingen { get; set; }
+        public byte[] Image { get; set; }
+        public int bedrijfsUren { get; set; }
+        public string aflegRedenen { get; set; }
+    }
+}
