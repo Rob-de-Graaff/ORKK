ebcd7071b9c6b6e254cc607f15084b8434869e95
Rob de Graaff <rob.degraaff@hotmail.com>
2020-03-24 11:56:42 +0100

    Added Kabelchecklist class

---
+++ OverdeRheinKraanKeuringen/Models/Kabelchecklist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OverdeRheinKraanKeuringen.Models
+{
+    public class Kabelchecklist
+    {
+        
+        public int kabelID { get; set; }
+        public int Breuk_6D { get; set; }
+        public int Breuk_30D { get; set; }
+        public int Beschadiging_Buitenzijde { get; set; }
+        public int Beschadiging_Roest_Corrosie { get; set; }
+        public int Verminderde_Kabeldiameter { get; set; }
+        public int Positie_Meetpunten { get; set; }
+        public int Beschadiging_Totaal { get; set; }
+        public int Type_Beschadiging_Roestvorming { get; set; }
+        
+        public int Opdrachtnummer { get; set; }
+        public virtual Opdracht Opdracht { get; set; }
+    }
+}
